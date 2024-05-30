d41ff3d13f6cd73d9eb3c410fd8c6c77a072f729
Mitchell Harris <mitchell.harris@mit.edu>
2024-05-30 18:00:59 -0400

    running on polynomials now

--- src/utils.jl
+++ src/utils.jl
@@ -9,11 +9,20 @@ Computes the integral of a monomial p over the sphere (surface)
 in n dimenions
 """
 function integrate(p::DynamicPolynomials.Monomial, n)
+    # evaluates to zero if any exponent is odd
+    if any(p.z .% 2 .== 1)
+        return 0
+    end
     i = 2 * prod([gamma(1/2 * (p.z[i] + 1)) for i = 1:lastindex(p.z)])
     i = i / gamma(1/2 * (sum(p.z) + n))
     return i
 end
-function integrate(p::Number, n) # catches the constant case
-    @polyvar _var
-    return integrate(_var^0, n)
+
+"""
+    function integrate(p::DynamicPolynomials.Polynomial, n) 
+Computes the integral of a polynomial p over the sphere (surface)
+in n dimenions
+"""
+function integrate(p::DynamicPolynomials.Polynomial, n)
+    return sum([integrate(p.x[i], n) * p.a[i] for i = 1:lastindex(p.x)])
 end
